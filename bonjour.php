ed99af9bff2fc541cecf423b451bf194853148c7
leo-hardy <40430728+leo-hardy@users.noreply.github.com>
2019-01-01 18:13:35 +0100

    Update bonjour.php

--- bonjour.php
+++ bonjour.php
@@ -1,8 +1,29 @@
-﻿<html>
+<html>
+
  <head>
   <title>Test PHP</title>
  </head>
+
  <body>
  <?php echo '<p>Hello world !</p>'; ?>
+
+ <button
+ onclick="myFunction1()">Créer la base
+ </button>
+
+ <button
+ onclick="myFunction2()">boutton 2 pas compris
+ </button>
+
+ <button
+ onclick="myFunction3()">bouton 3 pas compris
+ </button>
+
+ <button
+ onclick="myFunction4()">Effacer la base
+ </button>
+
+
  </body>
+
 </html>
